3a1f6bb30963aa227405689fbd2eb5f9bbc0eaf9
Yalla Shoot Live <84464758+yallashootlive@users.noreply.github.com>
2022-07-27 18:26:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,26 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>yalla shoot live</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Yalla Shoot Live" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         The first site to watch Arab and international matches and follow foreign leagues (English, Spanish, Italian and German)
+And also follow the Egyptian, Saudi and Moroccan league.
+<br>
+  <a href="https://uhd.live-yalla-shoot.com/">yalla shoot live</a>
+
         </p>
       </main>
 
-      <Footer />
+      <Footer>
+  <p className="description">
+    By Moka Templates
+    </p>
+  </Footer>
     </div>
   )
 }
